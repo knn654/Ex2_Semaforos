e6105960035b38822def85ae7f3d5cf0c5dc1bb6
knn654 <kaua.nascimento@fatec.sp.gov.br>
2023-03-23 16:01:00 -0300

    Correção de lógica

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,12 @@ public static Semaphore semaforo;
 	public static void main(String[] args) {
 		
 		int totalAvioes = 12;
-		semaforo = new Semaphore(1);
+		semaforo = new Semaphore(2);
 		
 		for (int i = 0; i < totalAvioes; i++) {
 			int pista = (int) (Math.random() * 2) + 1;
-			if(pista == 1) {
-				Thread decolagemNorte = new Threads(i, semaforo, pista);
-				decolagemNorte.start(); 
-			} else {
-				Thread decolagemSul = new Threads2(i, semaforo, pista);
-				decolagemSul.start();
-			}
+			Thread decolagem = new Threads(i, semaforo, pista);
+			decolagem.start();
 		}
 	}
 }
--- src/Threads.java
+++ src/Threads.java
@@ -10,8 +10,27 @@ public class Threads extends Thread{
 	}
 	
 	public void run() {
+		if (tipoPista == 1) {
+			System.out.println("Avião #" + numAviao + "| Pista: Norte");
+			norte();
+		} else {
+			System.out.println("Avião #" + numAviao + "| Pista: Sul");
+			sul();
+		}
 		System.out.println("Avião #" + numAviao + "| Pista: Norte");
 		norte();
+
+	}
+	
+	public void sul() {
+		try {
+			semaforo.acquire();
+			decolagemSul();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			semaforo.release();
+		}
 
 	}
 	
@@ -59,4 +78,37 @@ public class Threads extends Thread{
 		System.out.println("Pista norte liberada.");
 	}
 	
+	public void decolagemSul() {
+		System.out.println("Iniciando manobra do avião #" + numAviao + "| Pista: Sul");
+		System.out.println("Manobrando avião #" + numAviao + "...");
+		int tempoEspera = (int) (Math.random() * 401) + 300;
+		try {
+			Thread.sleep(tempoEspera);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Taxiando avião #" + numAviao + "...");
+		tempoEspera = (int) (Math.random() * 501) + 500;
+		try {
+			Thread.sleep(tempoEspera);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Avião #" + numAviao + " decolando...");
+		tempoEspera = (int) (Math.random() * 301) + 100;
+		try {
+			Thread.sleep(tempoEspera);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Avião #" + numAviao + " se afastando...");
+		tempoEspera = (int) (Math.random() * 501) + 300;
+		try {
+			Thread.sleep(tempoEspera);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Pista sul liberada.");	
+	}
+
 }
